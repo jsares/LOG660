1f440a6007f5f3f58e23b27ac616fe6c2c8c2133
Leandre Arseneault <leandre_arseneault@hotmail.fr>
2020-03-03 20:44:23 -0500

    Fermeture de la session à la fermeture de l'application.

--- src/ApplicationWindow.java
+++ src/ApplicationWindow.java
@@ -5,6 +5,8 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ApplicationWindow extends JFrame{
     private final CardLayout cl = new CardLayout();
@@ -44,6 +46,12 @@ public class ApplicationWindow extends JFrame{
                     frame.pack();
                     frame.setLocationRelativeTo(null);
                     frame.setVisible(true);
+                    frame.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosing(WindowEvent e) {
+                            frame.facade.closeSession();
+                            System.exit(0);
+                        }});
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -62,7 +70,7 @@ public class ApplicationWindow extends JFrame{
 
 		panel.add(searchBtn);
 		panel.setLayout(null);
-		
+
 		//Jtable setup
 		model = new DefaultTableModel();
 		table = new JTable(model);
@@ -70,106 +78,106 @@ public class ApplicationWindow extends JFrame{
 
 		model.addRow(facade.getEmptyRow());
 		table.setBounds(28, 67, 381, 148);
-              
+
 	    JScrollPane scroll = new JScrollPane(table);
 	    scroll.setBounds(311, 65, 297, 338);
 	    panel.add(scroll);
-	    
+
 	    //Search fields
 	    Font font = new Font("SansSerif", Font.PLAIN, 16);
-	    
+
 	    JTextArea movieSearchField = new JTextArea();
 	    movieSearchField.setBounds(131, 73, 157, 22);
         panel.add(movieSearchField);
 	    movieSearchField.setColumns(20);
 	    movieSearchField.setFont(font);
-	    
+
 	    JLabel lblTitleSearch = new JLabel("Titre");
 	    lblTitleSearch.setBounds(30, 76, 56, 16);
         panel.add(lblTitleSearch);
-	    
+
 	    JLabel lblSearchBy = new JLabel("Rechercher par");
 	    lblSearchBy.setBounds(88, 24, 200, 16);
         panel.add(lblSearchBy);
-	    
+
 	    JLabel lblFrom = new JLabel("Ann\u00E9e");
 	    lblFrom.setBounds(30, 123, 43, 16);
         panel.add(lblFrom);
-	    
+
 	    JTextArea fromTxtField = new JTextArea();
 	    fromTxtField.setFont(font);
 	    fromTxtField.setColumns(20);
 	    fromTxtField.setBounds(131, 117, 56, 22);
         panel.add(fromTxtField);
-	    
+
 	    JTextArea toTxtField = new JTextArea();
 	    toTxtField.setFont(font);
 	    toTxtField.setColumns(20);
 	    toTxtField.setBounds(232, 117, 56, 22);
         panel.add(toTxtField);
-	    
+
 	    JLabel ProdCountryLabel = new JLabel("Pays production");
 	    ProdCountryLabel.setBounds(30, 172, 100, 16);
         panel.add(ProdCountryLabel);
-	    
+
 	    JTextArea countryProdtextField = new JTextArea();
 	    countryProdtextField.setFont(font);
 	    countryProdtextField.setColumns(20);
 	    countryProdtextField.setBounds(131, 166, 157, 22);
         panel.add(countryProdtextField);
-	    
+
 	    JLabel lblLangue = new JLabel("Langue");
 	    lblLangue.setBounds(30, 222, 100, 16);
         panel.add(lblLangue);
-	    
+
 	    JTextArea langTextField = new JTextArea();
 	    langTextField.setFont(font);
 	    langTextField.setColumns(20);
 	    langTextField.setBounds(131, 216, 157, 22);
         panel.add(langTextField);
-	    
+
 	    JTextArea genreTextField = new JTextArea();
 	    genreTextField.setFont(font);
 	    genreTextField.setColumns(20);
 	    genreTextField.setBounds(131, 267, 157, 22);
         panel.add(genreTextField);
-	    
+
 	    JLabel lblGenre = new JLabel("Genre");
 	    lblGenre.setBounds(30, 273, 100, 16);
         panel.add(lblGenre);
-	    
+
 	    JLabel lblRealisateur = new JLabel("Realisateur");
 	    lblRealisateur.setBounds(30, 317, 100, 16);
         panel.add(lblRealisateur);
-	    
+
 	    JTextArea realisateurTxtField = new JTextArea();
 	    realisateurTxtField.setFont(font);
 	    realisateurTxtField.setColumns(20);
 	    realisateurTxtField.setBounds(131, 314, 157, 22);
         panel.add(realisateurTxtField);
-	    
+
 	    JLabel lblActors = new JLabel("Acteur");
 	    lblActors.setBounds(30, 367, 100, 16);
         panel.add(lblActors);
-	    
+
 	    JTextArea actorsTextField = new JTextArea();
 	    actorsTextField.setFont(font);
 	    actorsTextField.setColumns(20);
 	    actorsTextField.setBounds(131, 364, 157, 22);
         panel.add(actorsTextField);
-	    
+
 	    JLabel lblTitleDetail = new JLabel("Titre: ");
 	    lblTitleDetail.setBounds(662, 65, 43, 16);
         panel.add(lblTitleDetail);
-	    
+
 	    JLabel lblYearDetail = new JLabel("Ann\u00E9e: ");
 	    lblYearDetail.setBounds(662, 94, 62, 16);
         panel.add(lblYearDetail);
-	    
+
 	    JLabel ProdCountryLabelDetail = new JLabel("Pays production: ");
 	    ProdCountryLabelDetail.setBounds(660, 123, 100, 16);
         panel.add(ProdCountryLabelDetail);
-	    
+
 	    JLabel lblLangueDetail = new JLabel("Langue: ");
 	    lblLangueDetail.setBounds(662, 152, 50, 16);
         panel.add(lblLangueDetail);
@@ -237,11 +245,11 @@ public class ApplicationWindow extends JFrame{
 	    JLabel lblScenariste_1 = new JLabel("");
 	    lblScenariste_1.setBounds(745, 320, 585, 16);
         panel.add(lblScenariste_1);
-		
+
 		searchBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String[] result = facade.search(
-						movieSearchField.getText(), 
+						movieSearchField.getText(),
 						new String[] {fromTxtField.getText(), toTxtField.getText()},
 						countryProdtextField.getText(),
 						langTextField.getText(),
@@ -249,13 +257,13 @@ public class ApplicationWindow extends JFrame{
 						realisateurTxtField.getText(),
 						actorsTextField.getText()
 				);
-			
+
 				//Clear data in JTable
 				int rowCount = model.getRowCount();
 				for(int i = rowCount - 1; i >= 0; i--) {
 					model.removeRow(i);
 				}
-				
+
 				//Add data to JTable
 				for(int i=0; i<result.length;i++) {
 					model.addRow(new String[] {result[i]});
@@ -267,7 +275,7 @@ public class ApplicationWindow extends JFrame{
 				@Override
 				public void valueChanged(ListSelectionEvent e) {
 					SearchItems items = facade.handleRowClick(table.getValueAt(table.getSelectedRow(), 0).toString());
-					
+
 					lblTitleDetail_1.setText(items.getTitle());
 					lblYearDetail_1.setText(items.getYear()[0]);
 					ProdCountryLabelDetail_1.setText(items.getCountryProd());
